df2b43a1b488f6c7b46decf54f113073d38a601a
Matthew Gordon <matthew.scott.gordon@gmail.com>
2020-03-20 22:44:55 -0400

    Automatically detect column widths.

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,55 @@ use rustop::opts;
 use std::error::Error;
 use std::fs;
 
+fn autodetect_column_widths(input: &str, num_header_lines: usize) -> Vec<usize> {
+    let mut column_is_all_whitespace = vec![
+        true;
+        input
+            .lines()
+            .skip(num_header_lines)
+            .map(|l| l.len())
+            .max()
+            .unwrap_or(0)
+    ];
+    for line in input.lines().skip(num_header_lines) {
+        for (i, c) in line.char_indices() {
+            column_is_all_whitespace[i] &= c.is_whitespace();
+        }
+    }
+    /*izip!(
+            1..,
+            column_is_all_whitespace.iter(),
+            column_is_all_whitespace.iter().skip(1)
+    )*/
+    let column_starts: Vec<usize> = column_is_all_whitespace
+        .windows(2)
+        .zip(1..)
+        .flat_map(|(cs, i)| if !cs[0] && cs[1] { Some(i) } else { None })
+        .collect();
+    column_starts
+        .iter()
+        .cloned()
+        .take(1)
+        .chain(column_starts.windows(2).map(|w| w[1] - w[0]))
+        .collect()
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let (args, _) = opts! {
         opt input:String, desc:"Input filename";
     }
     .parse_or_exit();
 
+    let num_header_lines = 7;
+
     let input_contents = fs::read_to_string(args.input)?;
 
-    let num_header_lines = 8;
-    let percent_column_width = 8;
-    let address_column_width = 6;
+    let column_widths = autodetect_column_widths(&input_contents, num_header_lines);
 
     println!("<table>");
     for line in input_contents.lines().skip(num_header_lines) {
-        let (percent_str, rest) = line.split_at(percent_column_width.min(line.len()));
-        let (address_str, code_str) = rest.split_at(address_column_width.min(rest.len()));
+        let (percent_str, rest) = line.split_at(column_widths[0].min(line.len()));
+        let (address_str, code_str) = rest.split_at(column_widths[1].min(rest.len()));
 
         println!(
             "<tr><td>{}</td><td>{}</td><td>{}</td></tr>",
