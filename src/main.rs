09e80acf222f00bdf187d1ca34ebb967413b1b08
Matthew Gordon <matthew.scott.gordon@gmail.com>
2020-03-24 22:43:18 -0400

    First pass at syntax highlighting

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use rustop::opts;
 use std::error::Error;
 use std::fs;
 
+mod highlight;
+use highlight::Highlighter;
+
 fn autodetect_column_widths(input: &str, num_header_lines: usize) -> Vec<usize> {
     let mut column_is_all_whitespace = vec![
         true;
@@ -18,11 +21,6 @@ fn autodetect_column_widths(input: &str, num_header_lines: usize) -> Vec<usize> 
             column_is_all_whitespace[i] &= c.is_whitespace();
         }
     }
-    /*izip!(
-            1..,
-            column_is_all_whitespace.iter(),
-            column_is_all_whitespace.iter().skip(1)
-    )*/
     let column_starts: Vec<usize> = column_is_all_whitespace
         .windows(2)
         .zip(1..)
@@ -48,14 +46,27 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let column_widths = autodetect_column_widths(&input_contents, num_header_lines);
 
-    println!("<table>");
+    let highlighter = Highlighter::new();
+
+    println!(
+        "<table bgcolor=#{}>",
+        highlighter.get_background_color().as_hexadecimal()
+    );
     for line in input_contents.lines().skip(num_header_lines) {
         let (percent_str, rest) = line.split_at(column_widths[0].min(line.len()));
         let (address_str, code_str) = rest.split_at(column_widths[1].min(rest.len()));
 
+        let is_rust_line = !(address_str.contains(':') || code_str.trim().starts_with("_"));
+
         println!(
             "<tr><td>{}</td><td>{}</td><td>{}</td></tr>",
-            percent_str, address_str, code_str
+            percent_str,
+            address_str,
+            if is_rust_line {
+                highlighter.highlight_line(code_str)
+            } else {
+                code_str.to_string()
+            }
         );
     }
     println!("</table>");
